ff7e37ff6dcb46bcd8638ca54495f11ed08a0ddf
Ngoy banza <dngoyb@gmail.com>
2020-09-22 09:47:40 +0200

    added logic for hash change

--- tabs.js
+++ tabs.js
@@ -4,19 +4,32 @@
 
 const tabs = document.querySelectorAll('.tabs a');
 
+const showTabs = function (tab) {
+    const li = tab.parentNode;
+    const div = li.parentNode.parentNode;
+
+    if (!li.classList.contains('active')) {
+        div.querySelector('.tabs .active').classList.remove('active');
+        li.classList.add('active');
+
+        div.querySelector('.tab-content.active').classList.remove('active');
+        document
+            .querySelector(tab.getAttribute('href'))
+            .classList.add('active');
+    }
+    return false;
+};
+
 tabs.forEach((tab) => {
     tab.addEventListener('click', function (e) {
-        const li = this.parentNode;
-        const div = li.parentNode.parentNode;
-
-        if (!li.classList.contains('active')) {
-            div.querySelector('.tabs .active').classList.remove('active');
-            li.classList.add('active');
-
-            div.querySelector('.tab-content.active').classList.remove('active');
-            document
-                .querySelector(this.getAttribute('href'))
-                .classList.add('active');
-        }
+        showTabs(this);
     });
 });
+
+const hashChange = function () {
+    const hash = window.location.hash;
+    let a = document.querySelector(`a[href='${hash}']`);
+    showTabs(a);
+};
+
+window.addEventListener('hashchange', hashChange);
